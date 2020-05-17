73fcbb213ce6608ff01a143b756d03ed8c5daafc
alekseyivanov90 <54991556+alekseyivanov90@users.noreply.github.com>
2020-05-17 11:47:51 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+// Подключение всех модулей к программе
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 80
+
+app.listen(PORT);
+/*
+app.use('/file', express.static('file'));
+
+var server = require('https').createServer(app);
+var io = require('socket.io').listen(server);
+
+// Отслеживание порта
+// server.listen(3000);
+*/
+// Отслеживание url адреса и отображение нужной HTML страницы
+app.get('/', function(request, respons) {
+	respons.sendFile(__dirname + '/index.html');
+});
+/*
+// Массив со всеми подключениями
+connections = [];
+
+// Функция, которая сработает при подключении к странице
+// Считается как новый пользователь
+io.sockets.on('connection', function(socket) {
+	// console.log("Успешное соединение");
+	// Добавление нового соединения в массив
+	connections.push(socket);
+	// Функция, которая срабатывает при отключении от сервера
+	socket.on('disconnect', function(data) {
+		// Удаления пользователя из массива
+		connections.splice(connections.indexOf(socket), 1);
+		console.log("Отключились");
+	});
+
+	// Функция получающая сообщение от какого-либо пользователя
+	socket.on('send mess', function(data) {
+		// Внутри функции мы передаем событие 'add mess',
+		// которое будет вызвано у всех пользователей и у них добавиться новое сообщение 
+		io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+	});
+});*/
+/*
 var app = require('express')();
 // var http = require('http').Server(app);
 const PORT = process.env.PORT || 80
@@ -24,3 +68,4 @@ io.on('connection', function(socket){
         io.emit('chat message', msg);
     });
 });
+*/
