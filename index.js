e2a5c5043b9d320a331c5339f5a37893bdeec1c3
alekseyivanov90 <54991556+alekseyivanov90@users.noreply.github.com>
2020-05-31 13:22:25 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+const PORT = process.env.PORT || 80
+
+app.listen(PORT);
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use('/us', express.static('us'));
+
+app.get('/', function(request, respons) {
+	respons.sendFile(__dirname + '/index.html');
+});
+
+app.get('/go', function(request, respons) {
+	respons.sendFile(__dirname + '/go.html');
+});
+
+app.post('/inf', urlencodedParser, function(request, respons) {
+	var tt = request.body
+	fs.appendFileSync("us/hello.txt", "mstr.unshift('"+tt.opis+"'); \n");
+	fs.appendFileSync("us/hello.txt", "mssl.unshift('"+tt.sslk+"'); \n");
+	respons.sendFile(__dirname + '/index.html');
+	console.log(tt);
+});
